6ad90927c94aaaeb5675ea70c6de4a56a297994c
808Aidan <buffblueboy@gmail.com>
2016-06-30 11:20:47 -1000

    stuff
    fixed app

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-var games = ["Call of Duty 4" , "Fallout 4" , "Minecraft" , "Dark Souls Series" , "overwatch" ]
+var games = ["Call of Duty 4" , "Fallout 4" , "Minecraft" , "Dark Souls Series" , "overwatch" , "World of Warcraft" , "Doom" , "Halo" , "Battlefeild" , "Star Wars Battlefront" , "Madden 16" , "Fifa 16" , "NBA 2k16" , "Asphalt 8 Airborne"]
 
-function pickTopping(){
+function pickVideoGame(){
 	var gamesNumber = Math.floor(Math.random()*games.length);
 	var gamesNumber2 = Math.floor(Math.random()*games.length);
-	document.getElementById('pizza').innerHTML = "<h1>"+games[toppingNumber]+" and "+
+	document.getElementById('game').innerHTML = "<h1>"+games[gamesNumber]+" and "+
 	games[gamesNumber2]+"</h1>";
 	console.log(games[gamesNumber]);
 	console.log(gamesNumber);
 }
+
